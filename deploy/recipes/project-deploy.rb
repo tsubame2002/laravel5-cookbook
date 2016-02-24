67113e5318f9b9ba28d9b9944a774c00f9fb3f06
MasatoKume <tsubame2002@gmail.com>
2016-02-24 18:46:35 +0900

    deploy

--- deploy/recipes/project-deploy.rb
+++ deploy/recipes/project-deploy.rb
@@ -1,9 +1,18 @@
-app = search("aws_opsworks_app")
-app.each do
+include_recipe 'deploy'
+
+apps = search("aws_opsworks_app")
+apps.each do |app|
+  app_path = "/var/www/html/#{app['shortname']}"
 
 
   # Add write-access permission to "shared/log" directory.
   execute "test" do
     command "echo 'test'"
   end
+
+  git app_path do
+    repository app['app_source']['url']
+    action :sync
+  end
+
 end
