882f13ef5f3a4c4592ac8251366798571245337c
MasatoKume <tsubame2002@gmail.com>
2016-02-24 19:12:51 +0900

    tmp

--- deploy/recipes/project-deploy.rb
+++ deploy/recipes/project-deploy.rb
@@ -4,13 +4,10 @@ apps.each do |app|
   app_path = "/var/www/html/#{app['shortname']}"
 
 
-  # Add write-access permission to "shared/log" directory.
-  execute "test" do
-    command "echo 'test'"
-  end
 
   git app_path do
     repository app['app_source']['url']
+    deploy_key app['app_source']['ssh_key']
     action :sync
   end
 
